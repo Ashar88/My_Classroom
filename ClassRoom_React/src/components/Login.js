7a691cad4baba6ad532027bd6ee9dbcd72cb168b
MuhammadUmerHussain <umerhussain231123@gmail.com>
2022-11-08 02:53:48 +0500

    8-Nov-22, initial login progress
    i have designed login component via react but button of api google and use state is remaining

---
+++ ClassRoom_React/src/components/Login.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './index1.css';
+const Login = () => {
+  return (
+    <>
+        <body className='u'>
+        <div className="log">
+            <img src="https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=2000" className='ava' alt="" />
+            <form action=" " >
+            <h1 className='fo'>Login</h1>
+            <p className='p1'>Username</p>
+                <input type="text" name="" id="" className='bar'/>
+                <p className='p1'>Password</p>
+                <input type="text" name="" id="" className='bar'/>
+                <button className='bar2' >Submit </button>
+            </form>
+        </div>
+
+        </body>
+    </>
+  )
+}
+
+export default Login
