967ec4247fbe0db5c983f0abec7f63828d4b1ed3
MuhammadUmerHussain <umerhussain231123@gmail.com>
2022-11-22 01:43:01 +0500

    i have made second login according to requirements and deleted first one in this use state is working i have not fetch data by online api but i will add loader and fetch function when there will be online api, also i hae fetch data from my own created api for teting purpose thankyou

---
+++ ProjectDBFinalWork/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Login from './Login';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Login/>
+    </>
+  );
+}
+
+export default App;
---
+++ ProjectDBFinalWork/src/data.js
@@ -0,0 +1,9 @@
+const data=[{
+    name:'umer',
+    password:'umer'
+},
+{
+    name:'ashar',
+    password:'umer'
+},];
+export default data;
