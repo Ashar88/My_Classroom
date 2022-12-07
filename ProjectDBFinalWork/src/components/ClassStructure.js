919c3523b5226a4740d8dda23ac68e48ddfcb3b7
MuhammadUmerHussain <umerhussain231123@gmail.com>
2022-12-07 15:56:29 +0500

    Router issues are resolved by suing maintaining outlets in people2 and in outlet2 and using using use params in people stream and classwork to get id directly from link

---
+++ ProjectDBFinalWork/src/components/ClassStructure.js
@@ -0,0 +1,39 @@
+import React, { useEffect } from 'react'
+import People from './People'
+import NavBarClass from './NavBarClass'
+import { useGlobalContext } from '../context'
+import { useNavigate } from 'react-router-dom';
+import ClassWork from './ClassWork';
+import Stream from './Stream';
+const ClassStructure = () => {
+    const {navbarb,setnavbarb,classid,clickpeople,isclasswork}=useGlobalContext();
+    const nav=useNavigate();
+    console.log(classid)
+
+    useEffect(()=>{
+    <ClassStructure/>
+    },[isclasswork,clickpeople])
+  return (
+    <div>
+   
+   
+    <NavBarClass/>
+    <Stream/>
+    {
+      
+         
+       
+    }
+    {
+      
+       
+    }
+    {
+      
+    }
+      
+    </div>
+  )
+}
+
+export default ClassStructure
---
+++ ProjectDBFinalWork/src/components/Home2.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import {useGlobalContext} from '../context'
+import NavBarBox from './NavBarBox'
+import Abc from './Abc'
+import Box from './Box'
+import classdata from './classdata'
+import { Link } from 'react-router-dom'
+import './Box.css'
+import { useNavigate } from 'react-router-dom';
+
+const Home2 = () => {
+    const [data,setdata]=useState(classdata);
+     const{navbarb,setnavbarb,setclassid,classid}=useGlobalContext();
+     const nav=useNavigate();
+     const handleClick=(courseid)=>{
+
+        setclassid(courseid);
+        nav(`/class/${courseid}`);
+
+        
+
+     }
+  return (
+    
+    <div>
+    <NavBarBox/>
+    
+    <div class="main1">
+    {
+        data.map((curr)=>{
+            
+            {/* return<Link to={`/class/${curr.id}`}> <Box key={curr.id} {...curr} /></Link> */}
+            { return <button className='bttn' onClick={()=>{handleClick(curr.id)}}><Box key={curr.id} {...curr} /></button> }
+        })
+    }
+    
+        </div>
+    </div>
+  )
+}
+
+export default Home2
